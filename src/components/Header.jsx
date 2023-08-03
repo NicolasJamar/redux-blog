455eb64792c0444cbef18327de39e807495173a2
NicolasJamar <nicolas.jamar@gmail.com>
2023-08-03 16:48:33 +0200

    implement a count for optimosation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { increaseCount, getCount } from "../features/posts/postsSlice"
+
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const count = useSelector(getCount)
 
     return (
         <header className="Header">
@@ -11,6 +16,9 @@ const Header = () => {
                     <li><Link to="post">Add Post</Link></li>
                     <li><Link to="user">Users</Link></li>
                 </ul>
+                <button onClick={() => dispatch(increaseCount())}>
+                    {count}
+                </button>
             </nav>
         </header>
     )
